98b1f9788b2c07c8da0b118e6d3e4feda6496098
Your Name <hilde.t.kaasa@gmail.com>
2025-12-02 17:46:14 +0100

    Added users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get  "/settings/edit", to: "users#edit"
+  patch "/settings", to: "users#update"
+
 end
