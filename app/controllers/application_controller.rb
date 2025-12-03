c008047ec1d957748290aed485f8137db2dd870e
Your Name <hilde.t.kaasa@gmail.com>
2025-12-03 14:53:29 +0100

    Cleaned up the comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
       return super unless resource.is_a?(User)
-
+      
       if resource.physicals.blank? || resource.preferences.blank? || resource.allergies.blank?
         "/settings/edit"
       else
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
 
   # Settings-form for user preferences
   get  "/settings/edit", to: "users#edit"
