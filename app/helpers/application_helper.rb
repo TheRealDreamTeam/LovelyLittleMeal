b47ce15c7d7bf33f0e5957a01c4966053bafd51f
Robert Golusinski <robertgolusinski@gmail.com>
2025-12-07 19:29:32 +0100

    added meta and og tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # Generates absolute URL for Active Storage attachments (required for OG tags)
+  #
+  # @param attachment [ActiveStorage::Attached::One] The attached file
+  # @return [String] Absolute URL to the attachment
+  def absolute_url_for(attachment)
+    return nil unless attachment&.attached?
+    
+    # Use rails_blob_url which always generates absolute URLs
+    # In production with Cloudinary, this will use the Cloudinary CDN URL
+    # In development with local storage, this will use the app's host
+    rails_blob_url(attachment)
+  end
 end
